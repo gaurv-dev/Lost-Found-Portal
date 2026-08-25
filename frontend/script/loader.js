c96d81839a3205777226592dcd99b93a203b75c4
gaurv-web <gauravsoni1804@gmail.com>
2026-08-25 19:25:46 +0530

    add new files

--- frontend/script/loader.js
+++ frontend/script/loader.js
@@ -1,22 +1,22 @@
-async function loadComponent(id, file) {
-    const response = await fetch(file);
-    const html = await response.text();
-    document.getElementById(id).innerHTML = html;
-}
 
+async function loadComponent(id, file) {
+  const response = await fetch(file);
+  const html = await response.text();
+  document.getElementById(id).innerHTML = html;
+}
 async function loadLayout() {
 
-    await loadComponent("header", "pages/header.html");
-    await loadComponent("home", "pages/home.html");
-    await loadComponent("footer", "pages/footer.html");
+  await loadComponent("header", "header.html");
+  await loadComponent("home", "home.html");
+  await loadComponent("footer", "footer.html");
 
-    await loadComponent("reportModalContainer", "pages/report-modal.html");
-    await loadComponent("detailModalContainer", "pages/detail-modal.html");
+  await loadComponent("reportModalContainer", "report-modal.html");
+  await loadComponent("detailModalContainer", "detail-modal.html");
 
-    await loadComponent("hero", "pages/hero.html");
-    await loadComponent("stats", "pages/stats.html");
-    await loadComponent("filters", "pages/filters.html");
-    await loadComponent("items", "pages/items.html");
+  await loadComponent("hero", "hero.html");
+  await loadComponent("stats", "stats.html");
+  await loadComponent("filters", "filters.html");
+  await loadComponent("items", "items.html");
 
 }
 
